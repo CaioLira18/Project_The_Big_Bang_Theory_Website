cd66ec59da1c5839d9c2e49ea477e6bb377fda43
CaioFl18 <cflo@cesar.school>
2024-09-18 20:16:46 -0300

    Update 09/18

--- src/js/personagens.js
+++ src/js/personagens.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const personagem = botao.getAttribute("personagem");
 
             // Atualiza a imagem grande do personagem
-            document.getElementById("imagem-personagem").src = `/src/ImagemGrande/${personagem}.png`;
+            document.getElementById("imagem-personagem").src = `/src/ImagemPersonagem/${personagem}.png`;
 
             // Substitui o título e a biografia do personagem conforme necessário
             switch (personagem) {
@@ -30,11 +30,15 @@ document.addEventListener("DOMContentLoaded", function() {
                     break;
                 case "5":
                     document.querySelector(".personagem-title").textContent = "Howard";
-                    document.querySelector(".personagem-bio").textContent = "Howard Bio";
+                    document.querySelector(".personagem-bio").textContent = "Howard Wolowitz, interpretado por Simon Helberg, é um dos personagens principais de The Big Bang Theory. Ele é um engenheiro aeroespacial que se destaca por seu estilo único, frequentemente usando roupas coloridas e um famoso chapéu. Conhecido por seu senso de humor sarcástico e por suas piadas muitas vezes inusitadas, Howard também é um personagem que tem um grande amor por tecnologia e cultura pop.";
                     break;
                 case "6":
                     document.querySelector(".personagem-title").textContent = "Bernadette";
-                    document.querySelector(".personagem-bio").textContent = "Bernadette Bio";
+                    document.querySelector(".personagem-bio").textContent = "Bernadette Rostenkowski-Wolowitz, interpretada por Melissa Rauch, é uma das personagens centrais de The Big Bang Theory. Ela é uma microbiologista altamente inteligente e ambiciosa, conhecida por sua voz aguda e seu jeito assertivo. Inicialmente apresentada como colega de trabalho de Howard, sua relação evolui rapidamente para um romance, culminando em seu casamento.";
+                    break;
+                case "7":
+                    document.querySelector(".personagem-title").textContent = "Amy";
+                    document.querySelector(".personagem-bio").textContent = "Amy Farrah Fowler, interpretada por Mayim Bialik, é uma personagem fundamental em The Big Bang Theory. Introduzida como uma possível parceira para Sheldon, ela é uma neurocientista brilhante e possui uma personalidade peculiar que combina traços sociais inusitados com uma inteligência aguçada.";
                     break;
                 default:
                     // Caso padrão, se não houver correspondência
